d99a447f0ecb7e55ddf58a23676722f86756742a
Hector Barahona <hbarahona022@gmail.com>
2024-09-19 19:38:42 -0500

    text/links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -81,9 +81,8 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Personal Portfolio Websites"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              description="Designed and developed a responsive portfolio website. Key features include a toggleable navigation menu, smooth transitions, and a visually appealing portfolio grid. Achieved a modern design with a focus on responsiveness, interactivity, and visual appeal."
+              ghLink="https://github.com/hector-022b/myDevPort"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
