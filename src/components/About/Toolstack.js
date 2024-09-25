4a4e20e365bd000a177ecdf09b086ea2bf1c7408
Hector Barahona <hbarahona022@gmail.com>
2024-09-25 14:05:43 -0500

    Edited Tool Stack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,29 +2,49 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
+  SiGithub,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiWindows,
+  SiGit,
+  SiMicrosoftazure,
+  SiMysql,
+  SiFigma,
+  SiMicrosoftoffice,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+      <Col xs={4} md={2} className="tech-icons" title="Windows">
+        <SiWindows />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Visual Studio Code">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+      <Col xs={4} md={2} className="tech-icons" title="Github">
+        <SiGithub />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Slack">
         <SiSlack />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Vercel">
         <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Git">
+        <SiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Microsoft Azure">
+        <SiMicrosoftazure />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="MySql">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Figma">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Microsoft Office">
+        <SiMicrosoftoffice />
       </Col>
     </Row>
   );
