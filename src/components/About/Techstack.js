0d2e91893a60f5c5812b9a6e2632a008450bff6a
Hector Barahona <hbarahona022@gmail.com>
2024-09-25 15:52:36 -0500

    removed unused icons

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -11,14 +11,6 @@ import {
   DiPhp,
   DiCss3,
 } from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
