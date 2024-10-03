39cdb7f25c9b5024409a6533edce013db194bbbe
Hector Barahona <hbarahona022@gmail.com>
2024-10-02 21:33:37 -0500

    updated projects page with new links and imgs

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import classPort from "../../Assets/Projects/classPort.svg";
+import personalPort from "../../Assets/Projects/personalPort.svg";
+import AlienMan from "../../Assets/Projects/AlienMan.svg";
+import Dodger from "../../Assets/Projects/dodger.svg";
+import capstoneLogin from "../../Assets/Projects/capstoneLogin.svg";
+import bombMaze from "../../Assets/Projects/bombMaze.svg";
 
 function Projects() {
   return (
@@ -23,67 +23,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Dodger}
               isBlog={false}
               title="Dodger"
               description="Created my version of Dodger, a game where the player has to avoid falling objects. The player can move using the arrow keys or mouse. A score and top score are kept."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="" // No GitHub link provided, button will not show
+              demoLink="https://replit.com/@hbarahona022/dodger?v=1"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={bombMaze}
               isBlog={false}
               title="Bomb Maze"
               description="In this project I reskinned someone else's code to create my own game.  This game has a start phase, a play phase, and a game over phase. The player must navigate through the maze while dodging the bombs (the bombs are endless). There will be three rooms the player can play in. There is no time limit for the player to clear the room and the rooms loop."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="" // No GitHub link provided, button will not show
+              demoLink="https://replit.com/@hbarahona022/BombMaze?v=1"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={AlienMan}
               isBlog={false}
               title="AlienMan"
               description="Created my version of hangman. Learned to include a dictionary so there could be multiple word categories."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="" // No GitHub link provided, button will not show
+              demoLink="https://replit.com/@hbarahona022/alienMan?v=1"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={classPort}
               isBlog={false}
               title="Assignment Portfolio Website"
               description="Created a class projects website using HTML and CSS for a clean and straightforward showcase of academic work. The website features organized sections for different projects, maintaining a responsive design for various screen sizes."
-              // ghLink=""
+              ghLink="" // No GitHub link provided, button will not show
               demoLink="https://cpst.tulane.edu/~hbarahona/CPST2400StudentWebsiteStarterFiles/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={capstoneLogin}
               isBlog={false}
               title="Capstone"
               description="Led development for a web-enabled MVP, enabling users to browse the AFFES catalog, create a cart, and place orders seamlessly. Implemented role-based access control and addressed system flaws. Collaborated with cross-functional teams, ensuring project milestones and CMMC standards."
               ghLink="https://github.com/indigo77072/AppDevSouthBalanceSite"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              demoLink="https://docs.google.com/presentation/d/1zBgq7s8z6hZ77siOWrSBIKmFFpqu6Caqzt06lbwZlCw/edit?usp=sharing"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={personalPort}
               isBlog={false}
               title="Personal Portfolio Websites"
               description="Designed and developed a responsive portfolio website. Key features include a toggleable navigation menu, smooth transitions, and a visually appealing portfolio grid. Achieved a modern design with a focus on responsiveness, interactivity, and visual appeal."
               ghLink="https://github.com/hector-022b/myDevPort"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              demoLink="" // No Demo link provided, button will not show
             />
           </Col>
         </Row>
